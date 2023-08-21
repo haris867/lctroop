6bfc4497e7c6f8429d2384c63e62728760f8ef63
Haris <haris.usman@hotmail.com>
2023-08-21 03:42:38 +0200

    Fixed about us layout and added babel dependency

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -14,7 +14,7 @@ export default function About() {
           <span className="glow ms-2">-</span>
         </St.Heading>
       </div>
-      <Row className="d-flex justify-content-center">
+      <Row className="justify-content-center">
         <Col xs={12} sm={10} md={8} lg={6} xl={5}>
           <div className="d-flex justify-content-center align-items-center">
             <S.AboutCardTitle>OUR STORY</S.AboutCardTitle>
@@ -46,3 +46,44 @@ export default function About() {
     </Container>
   );
 }
+
+// <Container>
+//   {/* <Helmet>
+//       <title>Bits & Bots | About</title>
+//     </Helmet> */}
+//   <div className="d-flex justify-content-center">
+//     <St.Heading className="d-flex justify-content-center align-items-center pt-3">
+//       <span className="glow me-2">-</span>ABOUT US
+//       <span className="glow ms-2">-</span>
+//     </St.Heading>
+//   </div>
+//   <Col xs={12}>
+//     <Col xs={12} sm={10} md={8} lg={6} xl={5}>
+//       <div className="d-flex justify-content-center align-items-center">
+//         <S.AboutCardTitle>OUR STORY</S.AboutCardTitle>
+//         <S.AboutImage src="/images/about1.jpg" />
+//       </div>
+//       <S.AboutCardText>
+//         Founded by artist Young Cisto. LC TROOP ENTERTAINMENT AS is a record
+//         label / collective. While striving to become one of the greatest
+//         entertainment companies worldwide, the company offers a wide range
+//         of services from studio access for song recording / podcast
+//         recording, videography, photography and much more.
+//       </S.AboutCardText>
+//     </Col>
+//     <Col xs={12} sm={10} md={8} lg={6} xl={5}>
+//       <div className="d-flex justify-content-center align-items-center">
+//         <S.AboutCardTitle>OUR MISSION</S.AboutCardTitle>
+//         <S.AboutImage src="/images/about2.jpg" />
+//       </div>
+//       <S.AboutCardText>
+//         We empower our acts with values such as excellence, integrity and
+//         abundance. These principals are central in our company and each
+//         member is pushed to their limits to obtain their own version of
+//         success. Our goal is to entertain the masses & individuals with
+//         talents from our roster while creating job opportunities in the
+//         society.
+//       </S.AboutCardText>
+//     </Col>
+//   </Col>
+// </Container>;
--- src/pages/about/index.styles.js
+++ src/pages/about/index.styles.js
@@ -1,11 +1,4 @@
 import styled from "styled-components";
-import { Card } from "react-bootstrap";
-
-export const AboutCard = styled(Card)`
-  background-color: var(--color-secondary);
-  border: none;
-  font-family: "Roboto Mono", sans-serif;
-`;
 
 export const AboutImage = styled.img`
   border-radius: 0 !important;
@@ -14,13 +7,12 @@ export const AboutImage = styled.img`
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px !important;
   width: 90%;
   object-fit: cover;
-  object-fit: cover;
   aspect-ratio: 3/2;
 `;
 
 export const AboutCardText = styled.p`
   font-size: calc(0.9rem + 0.3vw) !important;
-  width: 90%;
+  width: 85%;
   margin: 0 auto;
   font-family: "Lao MN", sans-serif;
 `;
@@ -30,6 +22,5 @@ export const AboutCardTitle = styled.h2`
   position: absolute;
   z-index: 1;
   font-size: calc(1.7rem + 1.2vw) !important;
-  width: 100%;
   text-align: center;
 `;
